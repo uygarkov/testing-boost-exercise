25d9c420b74ed793de25beb55b5304cbf3efba6f
Uygar <83709209+uygarkov@users.noreply.github.com>
2023-02-08 17:44:44 +0100

    Add unit test for matrixIO::openData

---
+++ tests/matrixIOTest.cpp
@@ -0,0 +1,35 @@
+#define BOOST_TEST_DYN_LINk
+#include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
+#include "../src/matrixIO.cpp"
+#include <array>
+
+using namespace matrixIO;
+using namespace Eigen;
+
+struct matrixIOFixture {
+    matrixIOFixture()
+    {
+        filePath = "../data/m3.csv";
+        dimension = 3;
+        wrong_dimension = 5;
+        expected_m3 = MatrixXd(dimension, dimension);
+        expected_m3 << 0.680375, 0.59688, -0.329554,
+                    -0.211234, 0.823295, 0.536459,
+                    0.566198, -0.604897, -0.444451;
+    }
+    std::string        filePath;
+    int                dimension, wrong_dimension;
+    MatrixXd           expected_m3;
+};
+
+BOOST_FIXTURE_TEST_SUITE(matrixIOTests, matrixIOFixture)
+
+BOOST_AUTO_TEST_CASE(testOpenData)
+{
+    MatrixXd openData_m3 = openData(openFilename, dimension);
+    BOOST_CHECK_EQUAL(openData_m3, expected_m3);
+    BOOST_CHECK_THROW(openData(openFilename, wrong_dimension), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
